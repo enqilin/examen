9d88bc379cab86a47b1ad575f5546839f6a8da08
nad0us <114181724+enqilin@users.noreply.github.com>
2022-10-27 08:48:14 +0000

    pregunta 1

---
+++ main.py
@@ -0,0 +1,5 @@
+var_1 = "Módulo 1 de Python "
+var=var_1.lower
+longitud=len(var_1)
+division=longitud/7
+print(round(division,4))
--- pregunta1.py
+++ pregunta1.py
@@ -1,1 +1,17 @@
-var_1=
+var_1 = "Módulo 1 de Python "
+var=var_1.lower
+longitud=len(var_1)
+division=longitud/7
+print(round(division,4))
+def funcion1():
+  funcion=12-(4*2)-(2+2)
+  return funcion
+def funcion2():
+  funcion= 12-4*(2-2)+2
+  return funcion
+def funcion3():
+  edad=int(input("Introduce la edad que tienes:  " ))
+  if edad<18:
+    print("Menor de edad")
+  else:
+    print("Mayor de edad")
